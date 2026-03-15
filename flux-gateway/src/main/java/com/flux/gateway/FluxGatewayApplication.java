16dda0c5f808cbd84a65ef4059a77778a4de6de2
Michał <tarasiuk.michal@gmail.com>
2026-03-15 23:15:01 +0100

    feat: Task 1 - Gateway project skeleton with health endpoint

---
+++ flux-gateway/src/main/java/com/flux/gateway/FluxGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.flux.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxGatewayApplication.class, args);
+    }
+}
